65f00de87f1ed0a78bc139ff740ba7cb4eda059e
EduardoFFernandes <43785969+EduardoFFernandes@users.noreply.github.com>
2021-01-14 15:58:32 -0300

    Avancando as aulas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,8 +18,22 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule }from '@angular/material/form-field';
+import { MatInputModule }from '@angular/material/input';
 
+import { HttpClientModule } from '@angular/common/http';
 
+import { FormsModule }  from '@angular/forms';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common'
+
+registerLocaleData(localePt)
 
 @NgModule({
   declarations: [
@@ -30,6 +44,8 @@ import { MatButtonModule } from '@angular/material/button';
     HomeComponent,
     ProductCrudComponent,
     ProductCreateComponent,
+    ProductReadComponent,
+    ProductRead2Component
   ],
   imports: [
     BrowserModule,
@@ -40,9 +56,19 @@ import { MatButtonModule } from '@angular/material/button';
     MatListModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-br'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../product.model';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Router } from '@angular/router';
@@ -12,13 +13,19 @@ export class ProductCreateComponent implements OnInit {
   constructor(private productService: ProductService,
     private router: Router) { }
 
-  prop = "qualquer"
+  product: Product = {
+    name : '',
+    price : 0
+  }
 
   ngOnInit(): void {
     
   }
   createProduct(): void {
-    this.productService.ShowMessage('Hello World')
+    this.productService.createProduct(this.product).subscribe(() => {
+      this.productService.showMessage('Produto salvo!')
+      this.router.navigate(['/products'])
+    })
   }
   cancel(): void {
     this.router.navigate(['/products'])
